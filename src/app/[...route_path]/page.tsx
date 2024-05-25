003f0a5e25e16eedcef2884120f95cb3aa20881d
André Filipe Goulart <agoulart@desygner.com>
2024-05-24 22:44:09 -0300

    create types and slug pages

---
+++ src/app/[...route_path]/page.tsx
@@ -0,0 +1,3 @@
+export default function SlugPage(props: any) {
+  return <div>{JSON.stringify(props)}</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { API_BASE_URL } from "@/config";
-import { FilterApp } from "@/components/Filter";
+import Image from 'next/image';
+import styles from './page.module.css';
+import { API_BASE_URL } from '@/config';
+import { FilterApp } from '@/components/Filter';
+import { PlanetList } from '@/types/planet';
+import { ReactNode } from 'react';
 
 // Main
 //   - Filter navigation
@@ -12,11 +14,17 @@ import { FilterApp } from "@/components/Filter";
 //       - Name
 //       - Planet Name/Homeworld (use first item in `homeworld` <=> `http://swapi.dev/api/planets/`)
 
-export default async function Home(props: { searchParams: any }) {
-  const { planet } = props.searchParams || {};
+export default async function Home({
+  children,
+  ...props
+}: {
+  searchParams: any;
+  children: ReactNode;
+}) {
+  const { name } = props.searchParams || {};
 
-  const Home = await (
-    await fetch(`${API_BASE_URL}/planets${planet ? `/${planet}` : ""}`)
+  const planetList: PlanetList = await (
+    await fetch(`${API_BASE_URL}/planets`)
   ).json();
 
   return (
@@ -27,7 +35,9 @@ export default async function Home(props: { searchParams: any }) {
         <FilterApp />
       </div>
 
-      <div className={styles.grid}>{JSON.stringify(Home)}</div>
+      <div className={styles.grid}>{JSON.stringify(planetList.result)}</div>
+
+      {children}
     </main>
   );
 }
